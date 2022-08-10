283c2b0db81c04678c93f3eaa86f7e15b7c999f8
UmamageshwariCG <umamageshwari.n@capgemini.com>
2022-08-10 13:39:30 +0530

    day1

---
+++ day1/basic.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<sys/types.h>
+#include<unistd.h>
+using namespace std;
+int main()
+{
+	cout<<"PID : "<<getpid()<<endl;
+	cout<<"PPID : "<<getppid()<<endl;
+	for(;;);
+	return 0;
+}
